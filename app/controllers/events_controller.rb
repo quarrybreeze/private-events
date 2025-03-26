063c02173d5bf2e44a9a3570393e320e93c09298
Terrence Clayton <184171442+quarrybreeze@users.noreply.github.com>
2025-03-26 15:52:46 -0700

    added form for creating events, page for event details(show), added title and location to events for extra details. added destroy event funcitonality for creators.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,37 @@
 class EventsController < ApplicationController
   def index
-    @events = Event.all
+    @events = Event.includes(:creator).all
+  end
+
+  def show
+    @event = Event.includes(:creator).find(params[:id])
+    @current_user = current_user
+  end
+
+  before_action :authenticate_user!
+
+  def new
+    @event = current_user.created_events.new
+  end
+
+  def create
+    @event = current_user.created_events.build(event_params)
+
+    if @event.save
+      redirect_to root_path, notice: "Event was created successfully"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @event = Event.find(params[:id])
+    @event.destroy
+    redirect_to root_path, notice: "Event was deleted successfully"
+  end
+
+  private
+  def event_params
+    params.require(:event).permit(:when,:title,:location)
   end
 end
