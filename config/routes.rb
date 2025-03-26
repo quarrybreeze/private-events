6d044070a3eeb9a62ab1682a59c89308e30c3935
Terrence Clayton <184171442+quarrybreeze@users.noreply.github.com>
2025-03-26 13:32:14 -0700

    set up devise User and authentication. added name to user.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  # get "events/index"
+  devise_for :users
   root to: "events#index"
+  get "events/index"
   resources :events
+  resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
